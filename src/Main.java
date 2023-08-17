f289aeec40e127a3b273ad9b73794b05d1ce34d8
ilona gricevich <ilonagricevich123@gmail.com>
2023-08-17 22:46:59 +0300

    First part of homeworks

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+public class Main {
+        public static void main(String[] args) {
+
+      //  first_task();
+        System.out.println();
+     //   first_task_extra();
+        System.out.println();
+       // second_task_part1();
+            System.out.println();
+            second_task_part2();
+        }
+        static void first_task() {
+
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Поехали!");
+    }
+
+         static void first_task_extra() {
+
+             System.out.println("____888888______________________________");
+             System.out.println("____888888______________________________");
+             System.out.println("___8888888888___________________________");
+             System.out.println("__888888888888__________________________");
+             System.out.println("__8888111188888_________________________");
+             System.out.println("_888881111188888___11___________________");
+             System.out.println("_8888811111188888_11____________________");
+             System.out.println("_8888811111188888_11____________________");
+             System.out.println("_8888811111118888_11____________________");
+             System.out.println("_8888881111118888_11___11__8888888______");
+             System.out.println("_8888881111118888__1__11__88888888888___");
+             System.out.println("_8888888111111888____11__8888888888888__");
+             System.out.println("__888888111111888_111___88888888888888__");
+             System.out.println("__8888888111118881111__888881111118888__");
+             System.out.println("___888888811188881111_8888811111111888__");
+             System.out.println("____8888888188881111188888111111118888__");
+             System.out.println("_____88888888888111118888111111118888___");
+             System.out.println("______888888888111118888811111188888____");
+             System.out.println("______8888888881111188888888888888______");
+             System.out.println("_____888888888811111888888888888________");
+             System.out.println("____88888888888111118888888888__________");
+             System.out.println("___88881111888811111888888888___________");
+             System.out.println("__8888111111888111118888888888__________");
+             System.out.println("_888881111118881111188811118888_________");
+             System.out.println("_8888881111888811111881111118888________");
+             System.out.println("_8888888118888811111888111188888________");
+             System.out.println("__888888888888_1111888881188888_________");
+             System.out.println("___8888888888___111_8888888888__________");
+             System.out.println("____88888888_____1___88888888___________");
+             System.out.println("_____888888___________888888____________");
+             System.out.println("_______________________8888_____________");
+         }
+
+            static void second_task_part1() {
+
+            double a = 10;
+            double b = 12;
+
+            System.out.println("a+b = " + (a+b));
+            System.out.println("a*b = " + (a*b));
+            System.out.println("a-b = " + (a-b));
+            System.out.println("a/b = " + (a/b));
+            System.out.println("Остаток от деления а на b равен " + (a%b));
+            if (a%2==0) {
+                System.out.println("Число а четное");}
+                else {
+                System.out.println("Число а нечетное");
+                }
+                if (b%2==0) {
+                    System.out.println("Число b четное");}
+                else {
+                    System.out.println("Число b нечетное");
+                }
+            }
+
+    static void second_task_part2() {
+
+
+
+    }
+            }
+
+
